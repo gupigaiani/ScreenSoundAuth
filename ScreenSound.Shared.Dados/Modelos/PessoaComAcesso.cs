17b450d736ae4b3d10401210a3860831efdef573
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-03-30 17:32:44 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.Shared.Dados/Modelos/PessoaComAcesso.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Shared.Dados.Modelos;
+public class PessoaComAcesso : IdentityUser<int>
+{
+
+}
